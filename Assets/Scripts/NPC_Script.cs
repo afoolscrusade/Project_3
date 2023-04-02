c73ae05a2a3c0d70c34f9203f6b283974e0ee1e0
afoolscrusade <97545942+afoolscrusade@users.noreply.github.com>
2023-04-01 23:28:53 -0400

    Inventory is meh
    inventory is updated but not indicated. cannot use item yet...

--- Assets/Scripts/NPC_Script.cs
+++ Assets/Scripts/NPC_Script.cs
@@ -16,8 +16,22 @@ public class NPC_Script : MonoBehaviour
     }
 
     void Update()
+    
     {
-
+        //added pickup button to this script to make it work
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log("Pickup");
+            
+            float interactRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray)
+                if (collider.TryGetComponent(out ItemPickup ItemPickup))
+                {
+                    ItemPickup.Pickup();
+                    
+                }
+        }
     }
     
    
